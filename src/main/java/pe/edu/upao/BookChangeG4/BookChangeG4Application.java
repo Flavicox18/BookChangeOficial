61cf5dad785159dced5b7e0b32b62c6c3be67d33
Flavio Villacorta <143514047+Flavicox18@users.noreply.github.com>
2023-11-06 23:59:48 -0500

    First Commit

---
+++ src/main/java/pe/edu/upao/BookChangeG4/BookChangeG4Application.java
@@ -0,0 +1,13 @@
+package pe.edu.upao.BookChangeG4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookChangeG4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookChangeG4Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/upao/BookChangeG4/BookChangeG4ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upao.BookChangeG4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookChangeG4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
