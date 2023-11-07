07e8a8dbced5f8f2302cfdeadcc70263428c63fc
Frankcb04 <frankecb07@gmail.com>
2023-11-07 07:04:39 -0500

    Agregando US6, US7, US10

--- src/main/java/pe/edu/upao/bookchange/exception/PersonalizadoException.java
+++ src/main/java/pe/edu/upao/bookchange/exception/PersonalizadoException.java
@@ -6,7 +6,7 @@ import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import pe.edu.upao.BookChange.AgregarLibros.dto.ErrorResponseDto;
+import pe.edu.upao.bookchange.dto.ErrorResponseDto;
 
 import java.util.Date;
 import java.util.List;
